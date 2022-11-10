173ec3c6c075e02985fb1860e3faa438e7eb5694
yesol <ysoli36512@gmail.com>
2022-11-10 14:16:47 +0900

    ArgumentResolvers 활용하기

---
+++ src/main/java/hello/login/web/argumentresolver/Login.java
@@ -0,0 +1,10 @@
+package hello.login.web.argumentresolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+public @interface Login {
+
+
+}
